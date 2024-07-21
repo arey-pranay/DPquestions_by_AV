6046b06400c01bc95aea30623dc9da52485d9cab
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-07-21 20:46:54 +0530

    Create Tabulated.java

---
+++ Category-III/04. Length of SCS/Tabulated.java
@@ -0,0 +1,39 @@
+public class LongestCommonSubsequenceTabulated {
+
+    // Function to find the length of the Longest Common Subsequence (LCS)
+    public static int lcs(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Build the dp array from the bottom up
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0; // Base case: if one string is empty, LCS is 0
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1; // Characters match
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Characters don't match
+                }
+            }
+        }
+
+        // The value at dp[m][n] contains the length of LCS of str1 and str2
+        return dp[m][n];
+    }
+
+    // Function to find the length of the Shortest Common Supersequence (SCS)
+    public static int scsLength(String str1, String str2) {
+        int lcsLength = lcs(str1, str2);
+        int scsLength = str1.length() + str2.length() - lcsLength;
+        return scsLength;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "AGGTAB";
+        String str2 = "GXTXAYB";
+        System.out.println("Length of LCS is " + lcs(str1, str2));
+        System.out.println("Length of SCS is " + scsLength(str1, str2));
+    }
+}
