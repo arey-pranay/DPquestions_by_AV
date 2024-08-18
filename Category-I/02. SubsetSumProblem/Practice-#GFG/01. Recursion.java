ab75cef35e8c34c8de1e250605de7131b334491c
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-18 22:23:06 +0530

    Update 01. Recursion.java

--- Category-I/02. SubsetSumProblem/Practice-#GFG/01. Recursion.java
+++ Category-I/02. SubsetSumProblem/Practice-#GFG/01. Recursion.java
@@ -4,14 +4,16 @@ class Solution{
         // take or not take N
         // if you take N, sum becomes sum-N and i = i+1
         // if you do not take N, sum = sum and i = i+1, if sum == 0 return true;
-        return func(N, arr, sum, 0);
+        return func(N, arr, sum, 0) == 1 ? true : false;
+        //so the pointer (let's say, it is i) keeps changing and sum keeps changing, we will need 2D memo table in this
+        // int[][] memo = new int[sum+1][N+1];
     }
-    static Boolean func(int N, int[] arr, int sum, int i){
-        if(sum==0) return true;
-        if(sum<0) return false;
-        if(i==N) return false;
-        boolean take = func(N, arr, sum-arr[i], i+1);
-        boolean notTake = func(N, arr, sum, i+1);
-        return take || notTake; //so that if any call returns true, you will get true only
+    static int func(int N, int[] arr, int sum, int i){
+        if(sum==0) return 1;
+        if(sum<0) return 0;
+        if(i==N) return 0;
+        int take = func(N, arr, sum-arr[i], i+1);
+        int notTake = func(N, arr, sum, i+1);
+        return take | notTake; //so that if any call returns true, you will get true only
     }
 }
