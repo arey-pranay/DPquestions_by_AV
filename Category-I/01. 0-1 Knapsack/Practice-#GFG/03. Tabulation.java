a36b7a7154fb9af616aa0a2d250682531a5c7640
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-08-19 22:03:48 +0530

    Update 03. Tabulation.java

--- Category-I/01. 0-1 Knapsack/Practice-#GFG/03. Tabulation.java
+++ Category-I/01. 0-1 Knapsack/Practice-#GFG/03. Tabulation.java
@@ -1,28 +1,29 @@
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[]) {
-        //take or not take
-        int n = wt.length; // Number of items
-        int[][] dp = new int[n + 1][W + 1]; // DP table initialization
-        for (int i = 0; i <= n; i++) {
-            for (int w = 0; w <= W; w++) {
-                if (i == 0 || w == 0) {
-                    dp[i][w] = 0; // Base case: no items or zero capacity
-                } else if (wt[i - 1] <= w) {
-                    // Option 1: Include the item
-                    int include = val[i - 1] + dp[i - 1][w - wt[i - 1]];
-                    // Option 2: Exclude the item
-                    int exclude = dp[i - 1][w];
-                    dp[i][w] = Math.max(include, exclude);
-                } else {
-                    // If the item cannot be included
-                    dp[i][w] = dp[i - 1][w];
-                }
+    static int knapSack(int W, int[] wt, int[] val) {
+    int n = wt.length;
+    int Max = W;
+    int[][] dp = new int[Max + 1][n + 1];
+
+    // Fill the dp table
+    for (int i = 0; i < n; i++) {
+        for (int cap = 0; cap <= Max; cap++) {
+            if (wt[i] > cap) {
+                dp[cap][i + 1] = dp[cap][i]; // Can't take this item, so inherit the value without this item
+            } else {
+                int take = val[i] + dp[cap - wt[i]][i]; // Value if we take the item
+                int notTake = dp[cap][i]; // Value if we don't take the item
+                dp[cap][i + 1] = Math.max(take, notTake); // Max of taking or not taking the item
             }
         }
-        
-        return dp[n][W];
-        
-        
-      }
+    }
+    for(int[] arr : dp){
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    
+    // The result is in dp[W][n]
+    return dp[Max][n];
+
 }
